e1a4ea6e20a6d3713f63968bc6cf78246fba4e8c
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-02 09:19:29 +0000

    Remove python variable annotation folder

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-''' Floor module '''
-import math
-
-
-def floor(n: float) -> int:
-    ''' Takes a float n as argument and
-    returns the floor of the float.'''
-    return math.floor(n)
